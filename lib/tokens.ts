1fafa6f2be44444d7c298397c09e8160b53841c5
LOOIZIJIAN <looizijiann@gmail.com>
2024-05-27 22:03:23 +0800

    init: 2fa

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -2,6 +2,32 @@ import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
 import {v4 as uuidv4} from "uuid";
 import { db } from "./db";
+import crypto from "crypto";
+import { getTwoFactorTokenbyEmail } from "@/data/two-factor-token";
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+  const exisitingToken = await getTwoFactorTokenbyEmail(email);
+
+  if (exisitingToken) {
+    await db.twoFactorToken.delete({
+      where: {
+        id: exisitingToken.id,
+      },
+    });
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      token,
+      expires,
+      email,
+    },
+  });
+
+  return twoFactorToken;
+}
 
 export const generatePasswordResetToken = async (email: string) => {
   const token = uuidv4();
